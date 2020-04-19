5c4b1a601d1861e8ce4049c8fcd7dc34d73f9172
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-04-19 16:55:13 +0200

    Reformat code

--- src/main/java/sr/ice/server/CalcI.java
+++ src/main/java/sr/ice/server/CalcI.java
@@ -8,29 +8,32 @@ package sr.ice.server;
 //
 // **********************************************************************
 
-import com.zeroc.Ice.Current;
 import Demo.Calc;
+import com.zeroc.Ice.Current;
 
-public class CalcI implements Calc
-{
-	private static final long serialVersionUID = -2448962912780867770L;
+public class CalcI implements Calc {
 
-	@Override
-	public long add(int a, int b, Current __current) 
-	{
-		System.out.println("ADD: a = " + a + ", b = " + b + ", result = " + (a+b));
-		
-		if(a > 1000 || b > 1000) {
-			try { Thread.sleep(6000); } catch(InterruptedException ex) { } 
-		}
-		
-		if(__current.ctx.values().size() > 0) System.out.println("There are some properties in the context");
-		
-		return a + b;
-	}
-	
-	@Override
-	public long subtract(int a, int b, Current __current) {
-		return 0;
-	}
+    private static final long serialVersionUID = -2448962912780867770L;
+
+    @Override
+    public long add(int a, int b, Current __current) {
+        System.out.println("ADD: a = " + a + ", b = " + b + ", result = " + (a + b));
+
+        if (a > 1000 || b > 1000) {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException ex) {
+            }
+        }
+
+        if (__current.ctx.values().size() > 0)
+            System.out.println("There are some properties in the context");
+
+        return a + b;
+    }
+
+    @Override
+    public long subtract(int a, int b, Current __current) {
+        return 0;
+    }
 }
--- src/main/java/sr/ice/server/Server.java
+++ src/main/java/sr/ice/server/Server.java
@@ -9,73 +9,64 @@ package sr.ice.server;
 // **********************************************************************
 
 import com.zeroc.Ice.Communicator;
-import com.zeroc.Ice.Util;
-import com.zeroc.Ice.ObjectAdapter;
 import com.zeroc.Ice.Identity;
+import com.zeroc.Ice.ObjectAdapter;
+import com.zeroc.Ice.Util;
 
-public class Server
-{
-	public void t1(String[] args)
-	{
-		int status = 0;
-		Communicator communicator = null;
+public class Server {
 
-		try
-		{
-			// 1. Inicjalizacja ICE - utworzenie communicatora
-			communicator = Util.initialize(args);
+    public void t1(String[] args) {
+        int status = 0;
+        Communicator communicator = null;
 
-			// 2. Konfiguracja adaptera
-			// METODA 1 (polecana produkcyjnie): Konfiguracja adaptera Adapter1 jest w pliku konfiguracyjnym podanym jako parametr uruchomienia serwera
-			//Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Adapter1");  
-			
-			// METODA 2 (niepolecana, dopuszczalna testowo): Konfiguracja adaptera Adapter1 jest w kodzie Ÿród³owym
-			ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("Adapter1", "tcp -h localhost -p 10000:udp -h localhost -p 10000");
+        try {
+            // 1. Inicjalizacja ICE - utworzenie communicatora
+            communicator = Util.initialize(args);
 
-			// 3. Stworzenie serwanta/serwantów
-			CalcI calcServant1 = new CalcI();    
-			CalcI calcServant2 = new CalcI();    
-			
-						    
-			// 4. Dodanie wpisów do tablicy ASM
-			adapter.add(calcServant1, new Identity("calc11", "calc"));
-	        adapter.add(calcServant2, new Identity("calc22", "calc"));
+            // 2. Konfiguracja adaptera
+            // METODA 1 (polecana produkcyjnie): Konfiguracja adaptera Adapter1 jest w pliku konfiguracyjnym podanym jako parametr uruchomienia serwera
+            //Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Adapter1");
 
-	        
-			// 5. Aktywacja adaptera i przejœcie w pêtlê przetwarzania ¿¹dañ
-			adapter.activate();
-			
-			System.out.println("Entering event processing loop...");
-			
-			communicator.waitForShutdown(); 		
-			
-		}
-		catch (Exception e)
-		{
-			System.err.println(e);
-			status = 1;
-		}
-		if (communicator != null)
-		{
-			// Clean up
-			//
-			try
-			{
-				communicator.destroy();
-			}
-			catch (Exception e)
-			{
-				System.err.println(e);
-				status = 1;
-			}
-		}
-		System.exit(status);
-	}
+            // METODA 2 (niepolecana, dopuszczalna testowo): Konfiguracja adaptera Adapter1 jest w kodzie Ÿród³owym
+            ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("Adapter1", "tcp -h localhost -p 10000:udp -h localhost -p 10000");
+
+            // 3. Stworzenie serwanta/serwantów
+            CalcI calcServant1 = new CalcI();
+            CalcI calcServant2 = new CalcI();
 
 
-	public static void main(String[] args)
-	{
-		Server app = new Server();
-		app.t1(args);
-	}
+            // 4. Dodanie wpisów do tablicy ASM
+            adapter.add(calcServant1, new Identity("calc11", "calc"));
+            adapter.add(calcServant2, new Identity("calc22", "calc"));
+
+
+            // 5. Aktywacja adaptera i przejœcie w pêtlê przetwarzania ¿¹dañ
+            adapter.activate();
+
+            System.out.println("Entering event processing loop...");
+
+            communicator.waitForShutdown();
+
+        } catch (Exception e) {
+            System.err.println(e);
+            status = 1;
+        }
+        if (communicator != null) {
+            // Clean up
+            //
+            try {
+                communicator.destroy();
+            } catch (Exception e) {
+                System.err.println(e);
+                status = 1;
+            }
+        }
+        System.exit(status);
+    }
+
+
+    public static void main(String[] args) {
+        Server app = new Server();
+        app.t1(args);
+    }
 }
